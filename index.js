accd603068ecdf4b42e7c037157d55cc4efe67d1
Lucas Wiener <lucas@wiener.se>
2018-11-05 11:10:09 +0100

    First try

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactNative, {TextInput, NativeModules, NativeEventEmitter} from 'react-native';
+import PickerInputIos from './picker-input-ios';
+
+export PickerInputIos;
---
+++ picker-input-ios.js
@@ -0,0 +1,110 @@
+// This component enables the use of Picker keyboards for TextInput, as described by iOS guidelines https://developer.apple.com/ios/human-interface-guidelines/controls/pickers/
+// React Native does not currently support this, see https://github.com/facebook/react-native/issues/3551 and https://github.com/facebook/react-native/issues/5210
+//
+// Building upon https://github.com/DickyT/react-native-textinput-utils, the main idea is to:
+//
+// 1. Expose a picker-input buildingblock that renders a normal React Native TextInput buildingblock, passing along the props.
+// 2. When the TextInput buildingblock is mounted, invoke a native singleton object "EVRYPickerInput" that:
+// 2.a. Converts the given React TextInput reference to a UITextField pointer.
+// 2.b. Creates a UIPickerView and feeds it with the data given in :picker-items props.
+// 2.c. Register the UIPickerView to the .inputView property of the native UITextField screen.
+// 2.d. Reigster callbacks to UI events.
+//
+// Since it is a singleton object, UIDs are needed to identify the inputs between each other.
+// Callbacks are registered in a global state atom.
+
+import React, {Component} from 'react';
+import ReactNative, {TextInput, NativeModules, NativeEventEmitter} from 'react-native';
+
+var pickerInputHandler = NativeModules.EVRYPickerInputHandler; // WNRPickerInputHandler
+var pickerInputHandlerEventEmitter new NativeEventEmitter(pickerInputHandler);
+
+var counter = 1;
+
+function isPropsEqual(props1, props2) {
+    var items1 = props1.pickerItems;
+    var items2 = props2.pickerItems;
+    var value1 = props1.value;
+    var value2 = props2.value;
+
+    if (value1 !== value2) {
+        return false;
+    }
+
+    var l1 = items1.length;
+    var l2 = items2.length;
+    var lmax = l1 > l2 ? l1 : l2;
+
+    if (l1 !== l2) {
+        return false;
+    }
+
+    for (var i = 0; i < lmax; i++) {
+        var i1 = items1[i];
+        var i2 = items2[i];
+        if (i1.value !== i2.value || i1.label !== i2.label) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function getSelectedIndex(props) {
+    var index = props.pickerItems.map((p) => p.value).indexOf(props.value);
+    return index === -1 ? 0 : index;
+}
+
+function getSelectedLabel(props) {
+    for (var i = 0; i < props.pickerItems.length;) {
+        var item = props.pickerItems[i];
+        if (item.value === props.value) {
+            return item.label;
+        }
+    }
+    return null;
+}
+
+export default class PdfView extends Component {
+    constructor(props) {
+        super(props);
+
+        this._id = counter++;
+    }
+
+    componentDidMount(props) {
+        var thisComponent = this;
+        var nodeHandle = ReactNative.findNodeHandle(this.refs.input);
+        pickerInputHandler.componentDidMount(nodeHandle, {
+            pickerOptions: props.pickerOptions,
+            id: thisComponent._id,
+            selectedIndex: getSelectedIndex(props)
+        });
+        // "WNRPickerInputDidSelected"
+        this._didSelectedSubscription = pickerInputHandlerEventEmitter.addListener('EVRYKeyboardPickerViewDidSelected', function (data) {
+            if (props.onChange) {
+                if (data.id === thisComponent._id) {
+                    props.onChange(props.pickerItems[data.selectedIndex]);
+                }
+            }
+        })
+    }
+
+    componentWillReceiveProps(newProps) {
+        var currentProps = this.props;
+        if (!isPropsEqual(currentProps, newProps)) {
+            pickerInputHandler.componentWillReceiveProps(ReactNative.findNodeHandle(this.refs.input), {
+                pickerOptions: newProps.pickerOptions,
+                selectedIndex: getSelectedIndex(newProps)
+            });
+        }
+    }
+
+    render(props) {
+        return <TextInput {...props} ref="input" value={getSelectedLabel(props)} />;
+    }
+
+    componentWillUnmount() {
+        this._didSelectedSubscription.remove();
+    }
+}
